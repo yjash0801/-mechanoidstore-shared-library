33b883b3c3387faee9e5594390d49b39c3c304a4
yjash0801 <y.jashwanth5668@gmail.com>
2024-08-05 22:52:46 -0400

    Added global component for every microservice

--- vars/nodejsVM.groovy
+++ vars/nodejsVM.groovy
@@ -7,7 +7,8 @@ def call(Map configMap){
         }
         environment {
             packageVersion = ''
-            nexusURL = '172.31.38.156:8081'
+            // can maintain in pipeline globals
+            // nexusURL = '172.31.38.156:8081'
         }
         options {
             timeout(time: 1, unit: 'HOURS')
@@ -51,7 +52,7 @@ def call(Map configMap){
                 steps {
                     sh """
                         ls -la
-                        zip -q -r catalogue.zip ./* -x ".git" -x "*.zip"
+                        zip -q -r ${configMap.component}.zip ./* -x ".git" -x "*.zip"
                         ls -ltr
                     """
                 }
@@ -61,15 +62,15 @@ def call(Map configMap){
                     nexusArtifactUploader(
                         nexusVersion: 'nexus3',
                         protocol: 'http',
-                        nexusUrl: "${nexusURL}",
+                        nexusUrl: pipelineGlobals.nexusURL(),
                         groupId: 'com.mechanoidstore',
                         version: "${packageVersion}",
-                        repository: 'catalogue',
-                        credentialsId: 'nexus-auth',
+                        repository: "${configMap.component}",
+                        credentialsId: pipelineGlobals.nexusCredentials(),
                         artifacts: [
-                            [artifactId: 'catalogue',
+                            [artifactId: "${configMap.component}",
                             classifier: '',
-                            file: 'catalogue.zip',
+                            file: "${configMap.component}.zip",
                             type: 'zip']
                         ]
                     )
@@ -87,7 +88,7 @@ def call(Map configMap){
                             string(name: 'version', value: "$packageVersion"),
                             string(name: 'environment', value: "dev")
                         ]
-                        build job: "catalogue-deploy", wait: true, parameters: params
+                        build job: "${configMap.component}-deploy", wait: true, parameters: params
                     }
                 }
             }
---
+++ vars/pipelineGlobals.groovy
@@ -0,0 +1,7 @@
+static final String nexusURL(){
+    return "172.31.38.156"
+}
+
+static final String nexusCredentials(){
+    return "nexus-auth"
+}
